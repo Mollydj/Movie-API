9be4a57eccba70d535aa022451fa6edb20efd03a
Mollydj <55717647+Mollydj@users.noreply.github.com>
2020-02-19 23:21:22 -0500

    Removed additional ;

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const http = require('http'),
 fs = require('fs'),
-url = require ('');
+url = require('url');
 
-http.createServer((request,response) => {
+http.createServer((request, response) => {
   var addr = request.url,
   q = url.parse(addr, true),
   filePath = '';
 
-if (q.pathname.includes('documentation')) {
-  filePath = (__dirname + '/Users/mollydeangelis-jimenez/careerfoundry/movie_api/documentation.html');
-} else {
-  filePath ='index.html';
-}
-
-fs.readFile('/Users/mollydeangelis-jimenez/careerfoundry/movie_api/log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n',  function(err,data) {
-  if (err) {
-    throw err;
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath ='index.html';
   }
 
-response.writeHead(200, { 'Content-type': 'text/html'});
-respond.write(data);
-response.end();
-});
-
-}).listen(8080);
+  fs.readFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n',  function(err,data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  })
+}).listen(8080)
