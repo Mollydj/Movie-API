062a7f1b12211e8d930e1044bd20a9cd0aed809d
Mollydj <55717647+Mollydj@users.noreply.github.com>
2020-02-29 14:48:39 -0500

    Express, Middelware, and JSON added

--- index.js
+++ index.js
@@ -1,15 +1,80 @@
-const http = require('http'),
-  url = require('url');
+const express = require('express');
+const app = express();
 
-http.createServer((request, response) => {{
-  var requestURL = request.url;
-  if ( requestURL.pathname == "documentation.html")
-      response.writeHead(200, {'Content-Type': 'text/plain'};
-      response.end('Documentation on the bookclub API.\n');
-        } else {
-          response.writeHead(200, {'Content-type': 'text/plain'});
-          response.end('Welcome to my book club!\n');
-                }
-}).listen(8080);
+var myLogger = function (req, res, next){
+  console.log(req.url);
+  next();
+};
 
-console.log('My first Node test server is running on port 8080.');
+
+
+let topRomComs = [ {
+    title : 'About Time',
+    director : 'Richard Curtis'
+},
+{
+    title : '13 Going on 30',
+    director : 'Gary Winick'
+},
+{
+    title : 'Always Be My Maybe',
+    director : 'Nahnatchka Khan'
+},
+{
+    title : 'Sleeping With Other People',
+    director : 'Leslye Headland'
+},
+{
+    title : 'Four Weddings and a Funeral',
+    director : 'Richard Curtis'
+},
+{
+    title : 'Love Rosie',
+    director : 'Christian Ditter'
+},
+{
+    title : '500 Days of Summer',
+    director : 'Marc Webb'
+},
+{
+    title : 'Bridget Jones Diary',
+    director : 'Sharon Maguire'
+},
+{
+    title : 'The Big Sick',
+    director : 'Michael Showalter'
+},
+{
+    title : 'My Big Fat Greek Wedding',
+    director : 'Joel Zwick'
+}
+]
+
+
+//MIDDLEWARE
+app.use(express.static('public'));
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+app.use(myLogger);
+
+// GET requests
+app.get('/movies', function(req, res) {
+  res.json(topRomComs)
+});
+app.get('/', function(req, res) {
+  res.send('Welcome to my book club!')
+});
+app.get('/documentation', function(req, res) {
+  res.sendFile('public/documentation.html', { root : __dirname });
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
+});
+
+
+
+// listen for requests
+app.listen(8080, () =>
+  console.log('Your app is listening on port 8080.')
+);
