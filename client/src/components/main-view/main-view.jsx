e3e63206fd24d3fd2189b3a5641acf437fb43489
Mollydj <55717647+Mollydj@users.noreply.github.com>
2020-04-20 22:15:55 -0400

    updated babelrc

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component{
-constructor() {
-  super();
+export class MainView extends React.Component {
 
-  this.state = {};
-}
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('http://ach2.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-render() {
-  retrun (
-    <div className="main-view"></div>
-  )
-}
+
+  render() {
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
+    const { movies } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
+    return (
+     <div className="main-view">
+     { movies.map(movie => (
+       <div className="movie-card" key={movie._id}>{movie.Title}</div>
+     ))}
+     </div>
+    );
+  }
 }
