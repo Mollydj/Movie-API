2a8a2a02734b0f8795be57b042d9303db7e4174a
Mollydj <55717647+Mollydj@users.noreply.github.com>
2020-04-21 21:52:29 -0400

    updated access control allow origin

--- index.js
+++ index.js
@@ -26,19 +26,11 @@ require('./passport');
 
 
 const cors = require('cors')
-var allowedOrigins = ['*'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 //ERROR HANDLING
 
