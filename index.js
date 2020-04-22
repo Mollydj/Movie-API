b1d31c746382280a9de8992fbe0651e292c99223
Mollydj <55717647+Mollydj@users.noreply.github.com>
2020-04-21 22:30:28 -0400

    cors

--- index.js
+++ index.js
@@ -27,32 +27,15 @@ require('./passport');
 
 
 
-// Specifies that app uses CORS - default: allows requests from all origins
-app.use(cors());
+Access - Control - Allow - Origin: *
 
-// Allowing only certain origins to be given access
-var allowedOrigins = ['*'];
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+  //cors
+  //ERROR HANDLING
 
-//cors
-//ERROR HANDLING
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
 
 // GET //////////////////////////////////////////////
