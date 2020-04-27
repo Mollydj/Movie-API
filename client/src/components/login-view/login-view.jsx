7bfe651ac35afae53b92c59d25d720b314092fb4
Mollydj <55717647+Mollydj@users.noreply.github.com>
2020-04-27 15:04:41 -0400

    updated login url

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -14,7 +14,7 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault(); //prevents the default refresh of the page from your handlesubmit calling
-    axios.post('http://ach2.herokuapp.com/login', {
+    axios.post('/login', {
       Username: username,
       Password: password
     })
